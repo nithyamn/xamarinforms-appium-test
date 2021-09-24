deda39b47c23785f999893932c14c27d68582968
nithyamani <nithya.mn999@gmail.com>
2021-09-24 20:41:59 +0530

    webview related changes

--- SimpleApp.Appium.Core/TestMainPage.cs
+++ SimpleApp.Appium.Core/TestMainPage.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace SimpleApp.Appium.Core
 {
@@ -68,7 +73,89 @@ namespace SimpleApp.Appium.Core
         [Test()]
         public void TestAbout()
         {
+            waitForMobileElement((MobileBy)MobileBy.AccessibilityId("About"));
             appiumDriver.FindElement(MobileBy.AccessibilityId("About")).Click(); // works for iOS
+
+            waitForWebElement(MobileBy.XPath("//android.widget.Button[@text='LEARN MORE']"));
+            appiumDriver.FindElement(MobileBy.XPath("//android.widget.Button[@text='LEARN MORE']")).Click();
+
+            waitForWebElement(MobileBy.XPath("//android.widget.LinearLayout/android.widget.TextView[@text='Chrome']"));
+            appiumDriver.FindElement(MobileBy.XPath("//android.widget.LinearLayout/android.widget.TextView[@text='Chrome']")).Click();
+
+            waitForWebElement(MobileBy.Id("android:id/button_always"));
+            appiumDriver.FindElement(MobileBy.Id("android:id/button_always")).Click();
+            Thread.Sleep(3000);
+            List<string> AllContexts = new List<string>();
+            foreach (var context in (appiumDriver.Contexts))
+            {
+                AllContexts.Add(context);
+            }
+            Console.WriteLine("Context count: " + AllContexts.Count+" Contains Webview? "+AllContexts.Contains("WEBVIEW"));
+
+            appiumDriver.Context = "WEBVIEW_chrome";
+            waitForWebElement(By.CssSelector(".navbar-toggler"));
+            appiumDriver.FindElement(By.CssSelector(".navbar-toggler")).Click();
+
+            waitForWebElement(By.XPath("//*[@id='navbarSupportedContent']/ul/li[3]/a"));
+            appiumDriver.FindElement(By.XPath("//*[@id='navbarSupportedContent']/ul/li[3]/a")).Click();
+
+            waitForWebElement(By.CssSelector("#navbarSupportedContent"));
+            //waitForWebElement(By.CssSelector("#xamarin-forms"));
+
+            IWebElement scrollElement = appiumDriver.FindElement(By.CssSelector("#navbarSupportedContent"));
+            scrollToElementInWebView(scrollElement);
+        }
+
+        public void waitForMobileElement(MobileBy mobileBy)
+        {
+            try
+            {
+                //ExpectedConditions has been deprecated.
+                //another approach is to rely on additional plugin SeleniumExtras.WaitHelpers.ExpectedConditions
+                WebDriverWait wait = new WebDriverWait(appiumDriver, TimeSpan.FromSeconds(30));
+                wait.Until(c => c.FindElement(mobileBy).Displayed);
+            }
+            catch(Exception e)
+            {
+                //Console.WriteLine(e.Message);
+                throw new Exception("EXCEPTION - Could not find Native element - "+e);
+            }
+        }
+        public void waitForWebElement(By webBy)
+        {
+            try
+            {
+                //ExpectedConditions has been deprecated.
+                //another approach is to rely on additional plugin SeleniumExtras.WaitHelpers.ExpectedConditions
+                WebDriverWait wait = new WebDriverWait(appiumDriver, TimeSpan.FromSeconds(30));
+                wait.Until(c => c.FindElement(webBy).Displayed);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("EXCEPTION - Could not find Web element - "+e);
+                //Console.WriteLine(e.Message);
+            }
+        }
+        public void scrollToElementInWebView(IWebElement webElement)
+        {
+            //Console.WriteLine("Scrolling....");
+            Actions actions = new Actions(appiumDriver);
+            //actions.KeyDown(Keys.Control).SendKeys(Keys.End).Perform();
+            actions.MoveToElement(webElement);
+            actions.Perform();
+
+            //IJavaScriptExecutor jse = (IJavaScriptExecutor)appiumDriver;
+            //jse.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+            //jse.ExecuteScript("arguments[0].scrollIntoView();", appiumDriver.FindElement(By.CssSelector("#navbarSupportedContent")));
+            //Console.WriteLine("Scrolling done!");
+        }
+        public void scrollToElementInNativeView(AndroidElement androidElement)
+        {
+            Console.WriteLine("Scrolling....");
+            TouchActions actions = new TouchActions(appiumDriver);
+            actions.MoveToElement(androidElement);
+            actions.Perform();
+            Console.WriteLine("Scrolling done!");
         }
     }
 }
--- SimpleApp/SimpleApp.Android/MainActivity.cs
+++ SimpleApp/SimpleApp.Android/MainActivity.cs
@@ -21,6 +21,7 @@ namespace SimpleApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Android.Webkit.WebView.SetWebContentsDebuggingEnabled(true);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
